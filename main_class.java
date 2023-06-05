54bdccdd0750b3c05b946cbbe1a346cab3a47847
Eduardo Pinheiro <111510684+eduardohpinheiro@users.noreply.github.com>
2023-06-05 20:00:37 -0300

    Add files via upload

---
+++ main_class.java
@@ -0,0 +1,31 @@
+package atividade_arraylist;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Atividade_arraylist {
+
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      
+      Curso curso; 
+      ArrayList<Curso> cursos = new ArrayList<>();
+      
+      Aluno aluno = new Aluno();
+      ArrayList<Double> notas = new ArrayList<>();
+      
+      Telefone telefone;
+      ArrayList<Telefone> telefones = new ArrayList<>();
+      
+      Professor novoProf = new Professor();
+      Endereco endereco;
+      
+      System.out.println("___CADASTRO PROFESSOR___");
+      
+      System.out.println("Digite o nome: ");
+      
+      
+      
+    }
+    
+}
